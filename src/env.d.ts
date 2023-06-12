88fda10511383451bbb63d1db3388236ae39ce6d
Shinsina <shinsina@att.net>
2023-06-11 21:24:35 -0500

    Update ImportMetaEnv interface

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,14 +1,7 @@
 /// <reference types="astro/image/client" />
 interface ImportMetaEnv {
-  readonly FIREBASE_PRIVATE_KEY_ID: string;
-  readonly FIREBASE_PRIVATE_KEY: string;
-  readonly FIREBASE_PROJECT_ID: string;
-  readonly FIREBASE_CLIENT_EMAIL: string;
-  readonly FIREBASE_CLIENT_ID: string;
-  readonly FIREBASE_AUTH_URI: string;
-  readonly FIREBASE_TOKEN_URI: string;
-  readonly FIREBASE_AUTH_CERT_URL: string
-  readonly FIREBASE_CLIENT_CERT_URL: string;
+  readonly MONGODB_URI: string
+  readonly MONGODB_DB: string
 }
 
 interface ImportMeta {
