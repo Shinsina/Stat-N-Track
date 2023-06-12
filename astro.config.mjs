6c49bce5f00280fedca49b49293870e65e1e24e9
Shinsina <shinsina@att.net>
2023-06-11 21:31:49 -0500

    Use regular functions over the edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
-import netlify from "@astrojs/netlify/edge-functions";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
