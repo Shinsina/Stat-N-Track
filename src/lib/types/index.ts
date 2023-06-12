6832d99ce5dec49452218030e4c50b8e356d77c9
Shinsina <shinsina@att.net>
2023-06-11 21:23:18 -0500

    Add License type to index

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,3 +1,9 @@
+export type License = {
+  min_license_level: number,
+  license_group: number,
+  group_name: string,
+};
+
 export type Session = {
   simsession_name: string,
   simsession_type_name: string,
