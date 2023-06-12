045389b7b71d10a8e00f3133516fe80362900183
Shinsina <shinsina@att.net>
2023-06-11 21:24:03 -0500

    MongoDB client connection

---
+++ src/lib/mongodb.js
@@ -0,0 +1,43 @@
+import { MongoClient } from 'mongodb';
+
+if (!import.meta.env.MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable inside a root .env file');
+}
+
+if (!import.meta.env.MONGODB_DB) {
+  throw new Error('Please define the MONGODB_DB environment variable inside a root .env file');
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongo;
+
+if (!cached) {
+  cached = global.mongo = { conn: null, promise: null };
+}
+
+export async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+
+    // @ts-ignore
+    cached.promise = MongoClient.connect(import.meta.env.MONGODB_URI, opts).then((client) => {
+      return {
+        client,
+        db: client.db(import.meta.env.MONGODB_DB),
+      };
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
