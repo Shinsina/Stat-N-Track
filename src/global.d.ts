f3be38d47a79ccba7311ae9905ffdbd159be049b
Jake Collins <shinsina@att.net>
2023-07-07 12:00:57 -0500

    Correct indentation

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,9 +2,9 @@ import { MongoClient, Db } from "mongodb";
 
 declare global {
   namespace globalThis {
-      var mongo: {
-        conn: { client: MongoClient; db: Db; } | null;
-        promise: Promise<{ client: MongoClient; db: Db; }> | null;
-      };
+    var mongo: {
+      conn: { client: MongoClient; db: Db; } | null;
+      promise: Promise<{ client: MongoClient; db: Db; }> | null;
+    };
   }
 }
