cc13c52c91da493ce44634d8f8ba31f0ef297750
Jake Collins <shinsina@att.net>
2023-07-12 13:48:44 -0500

    Properly parse lap times a minute or longer

--- src/lib/utils/parse-lap-time.ts
+++ src/lib/utils/parse-lap-time.ts
@@ -1,11 +1,34 @@
-export default (averageLap: number): number => {
-	const ingestedAverageLap = String(averageLap);
-	const decimal = String(ingestedAverageLap.match(/[0-9]{4}$/));
-	const withDecimal = ingestedAverageLap.replace(decimal, `.${decimal}`);
+export default (lap: number): string => {
+	const ingestedLap = String(lap);
+	const placeDecimal = () => {
+		switch(ingestedLap.length){
+			case 1:
+				return `0.000${ingestedLap}`;
+			case 2:
+				return `0.00${ingestedLap}`;
+			case 3:
+				return `0.0${ingestedLap}`;
+			case 4:
+				return `0.${ingestedLap}`;
+			default:
+				const decimal = ingestedLap.match(/[0-9]{4}$/);
+				return ingestedLap.replace(String(decimal), `.${decimal}`);
+		}
+	};
+	const withDecimal = lap > 0 ? placeDecimal() : ingestedLap;
 	const [priorToDecimal, _] = withDecimal.split('.');
 	if (priorToDecimal.length > 2) {
 		const seconds = String(priorToDecimal.match(/[0-9]{2}$/));
 		withDecimal.replace(seconds, `.${seconds}`);
 	}
-	return Number(withDecimal.replace(/0$/, '').trim());
+	const numericalValue = Number(withDecimal.replace(/0$/, '').trim());
+	// If the lap is a minute or longer
+	if (numericalValue >= 60) {
+		const minutes = Math.floor(numericalValue / 60);
+		const seconds = Math.floor(numericalValue - (minutes * 60));
+		const subseconds = withDecimal.split('.').pop();
+		const secondsString = seconds >= 10 ? seconds : `0${seconds}`;
+		return `${minutes}:${secondsString}.${subseconds}`
+	}
+	return withDecimal.replace(/0$/, '').trim();
 }
