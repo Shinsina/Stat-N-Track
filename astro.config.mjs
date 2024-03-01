ef4ebe5d003a7f564ff6e143fb66072659d7524c
Shinsina <shinsina@att.net>
2024-03-01 16:12:29 -0600

    Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://shinsina.github.io",
   base: "/Stat-N-Track",
-  integrations: [tailwind(), prefetch()]
+  integrations: [tailwind(), prefetch(), sitemap()]
 });
