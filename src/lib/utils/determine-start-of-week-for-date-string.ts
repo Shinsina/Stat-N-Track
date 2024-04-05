4d5f97481900465d2dd1e94bd2d190904d11c169
Shinsina <shinsina@att.net>
2024-04-05 17:50:50 -0500

    Include determine-start-of-week-for-date-string-utility

---
+++ src/lib/utils/determine-start-of-week-for-date-string.ts
@@ -0,0 +1,25 @@
+export default ({
+  object,
+  dateString,
+}: {
+  object: Record<string, Array<string>>;
+  dateString: string;
+}) => {
+  const d = new Date(String(dateString));
+  const day = d.getDay();
+  const utcDateString = d.toUTCString();
+  if (day === 1 && !object[utcDateString]) {
+    object[utcDateString] = [utcDateString];
+  } else if (day !== 1) {
+    const distanceFromOne = day - 1;
+    const dayOneOfWeek = new Date(
+      Number(d) - distanceFromOne * 86400000
+    ).toUTCString();
+    if (object[dayOneOfWeek]) {
+      object[dayOneOfWeek].push(utcDateString);
+    } else {
+      object[dayOneOfWeek] = [utcDateString];
+    }
+  }
+  return object;
+};
