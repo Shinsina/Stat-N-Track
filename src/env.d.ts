74d2760908784a3196a5d732bc3d260072ddca4e
Shinsina <shinsina@att.net>
2024-05-03 11:49:58 -0500

    Remove mongodb related configuration

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,14 +1,6 @@
 /// <reference path="../.astro/db-types.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-interface ImportMetaEnv {
-  readonly MONGODB_URI: string;
-  readonly MONGODB_DB: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
 
 interface Window {
   Alpine: import('alpinejs').Alpine;
