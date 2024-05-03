cea91922d53dc40070e6fbc4e078aa603cedeedc
Shinsina <shinsina@att.net>
2024-05-03 12:26:42 -0500

    Define non-race-session-keys-to-display Set

---
+++ src/lib/utils/non-race-session-keys-to-display.ts
@@ -0,0 +1,17 @@
+export default new Set([
+  "cust_id",
+  "display_name",
+  "finish_position",
+  "finish_position_in_class",
+  "laps_complete",
+  "interval",
+  "class_interval",
+  "average_lap",
+  "best_lap_num",
+  "best_lap_time",
+  "car_class_short_name",
+  "club_shortname",
+  "division_name",
+  "incidents",
+  "car_name",
+]);
