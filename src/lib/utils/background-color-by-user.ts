04ca8dd5d0a4348461ffc92e135f5c3aa6199e37
Shinsina <shinsina@att.net>
2024-09-12 10:30:43 -0500

    Utilize maps for determining background color, add new users

--- src/lib/utils/background-color-by-user.ts
+++ src/lib/utils/background-color-by-user.ts
@@ -1,25 +1,27 @@
-export const bgColorByUserId = (userId: number): string => {
-  if (userId === 251134) {
-    return "bg-red-700";
-  }
-  if (userId === 300752) {
-    return "bg-orange-700";
-  }
-  if (userId === 815162) {
-    return "bg-purple-700";
-  }
-  return "";
-};
+const userIdMap: Map<number, string> = new Map([
+  [182407, "bg-amber-700"],
+  [188056, "bg-gray-700"],
+  [251134, "bg-red-700"],
+  [300752, "bg-orange-700"],
+  [589449, "bg-blue-700"],
+  [714312, "bg-yellow-700"],
+  [746377, "bg-green-700"],
+  [815162, "bg-purple-700"],
+]);
 
-export const bgColorByUserDisplayName = (displayName: string): string => {
-  if (displayName === "Kyle Klendworth") {
-    return "bg-red-700";
-  }
-  if (displayName === "Jacob Collins") {
-    return "bg-orange-700";
-  }
-  if (displayName === "Jack Glenzinski") {
-    return "bg-purple-700";
-  }
-  return "";
-};
+const displayNameMap: Map<string, string> = new Map([
+  ["Antonio Estrada", "bg-amber-700"],
+  ["Doug South", "bg-gray-700"],
+  ["Kyle Klendworth", "bg-red-700"],
+  ["Jacob Collins", "bg-orange-700"],
+  ["Bryan Campbell2", "bg-blue-700"],
+  ["Sam Karasala", "bg-yellow-700"],
+  ["Ty Quila", "bg-green-700"],
+  ["Jack Glenzinski", "bg-purple-700"],
+]);
+
+export const bgColorByUserId = (userId: number): string =>
+  userIdMap.get(userId) || "";
+
+export const bgColorByUserDisplayName = (displayName: string): string =>
+  displayNameMap.get(displayName) || "";
